db4c4864af380960fc6ddb647b5beb78a1f578a4
shani bider <shanibider@gmail.com>
2023-04-08 23:22:46 +0300

    Update Home screen + transactions + BottomAppBar

--- app/src/main/java/com/example/wallety/HomeFragment.java
+++ app/src/main/java/com/example/wallety/HomeFragment.java
@@ -9,6 +9,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +18,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.braintreepayments.cardform.view.CardForm;
+import com.example.wallety.adapters.HomeAdapter;
 import com.example.wallety.databinding.FragmentHomeBinding;
+import com.example.wallety.model.Transactions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
     FragmentHomeBinding binding;
+    RecyclerView recyclerView;
+    List<Transactions> transactionsList;
+    HomeAdapter homeAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +46,22 @@ public class HomeFragment extends Fragment {
         binding.savingMoneyCv.setOnClickListener(view1 -> {
             Navigation.findNavController(view1).navigate(R.id.action_homeFragment_to_savingMoneyFragment);
         });
+
+
+        recyclerView = view.findViewById(R.id.transactions_recList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        transactionsList = new ArrayList<>();
+
+        transactionsList.add(new Transactions(R.drawable.shopping_cart, "Super market", "08/04/2023", " - ₪159"));
+        transactionsList.add(new Transactions(R.drawable.shopping_bag, "KSP", "08/04/2023", " - ₪299"));
+        transactionsList.add(new Transactions(R.drawable.shopping_cart, "AM PM", "07/04/2023", " - ₪79"));
+        transactionsList.add(new Transactions(R.drawable.shopping_bag, "Shopping", "03/04/2023", " - ₪250"));
+        transactionsList.add(new Transactions(R.drawable.parents_transfer, "Mom transfer", "01/04/2023", " + ₪200"));
+
+        homeAdapter= new HomeAdapter(getContext(), transactionsList);
+        recyclerView.setAdapter(homeAdapter);
+        homeAdapter.notifyDataSetChanged();
+
 
         return view;
     }
---
+++ app/src/main/java/com/example/wallety/model/Transactions.java
@@ -0,0 +1,50 @@
+package com.example.wallety.model;
+
+public class Transactions {
+
+    int image;
+    private String source;
+    private String date;
+    private String sum;
+
+
+
+    public Transactions(int image, String source, String date, String sum) {
+        this.image = image;
+        this.source = source;
+        this.date = date;
+        this.sum = sum;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSum() {
+        return sum;
+    }
+
+    public void setSum(String sum) {
+        this.sum = sum;
+    }
+}
