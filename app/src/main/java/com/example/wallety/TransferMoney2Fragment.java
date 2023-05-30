459b6f97a14b2b87ef98cc77d134da322d976084
shani bider <shanibider@gmail.com>
2023-05-30 16:51:24 +0300

    Your commit message

---
+++ app/src/main/java/com/example/wallety/TransferMoney2Fragment.java
@@ -0,0 +1,72 @@
+package com.example.wallety;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.wallety.databinding.FragmentTransferMoney2Binding;
+import com.example.wallety.databinding.FragmentTransferMoneyBinding;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.Objects;
+
+public class TransferMoney2Fragment extends Fragment {
+    final String FIRST_AMOUNT_OPTION = "50";
+    final String SECOND_AMOUNT_OPTION = "100";
+    final String THIRD_AMOUNT_OPTION = "150";
+    FragmentTransferMoney2Binding binding;
+    String firstAmountOption = FIRST_AMOUNT_OPTION;
+
+    EditText t1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+           // Inflate the layout for this fragment
+           binding = FragmentTransferMoney2Binding.inflate(inflater, container, false);
+           View view = binding.getRoot();
+
+           // hide actionBar
+           ActionBar actionBar = null;
+           try {
+               actionBar = Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar());
+           } catch (NullPointerException e) {}
+           if (actionBar != null) {
+               actionBar.hide();
+           }
+
+           binding.transferBtn.setOnClickListener(view1 -> {
+               Navigation.findNavController(view1).navigate(R.id.action_transferMoneyFragment2_to_moneySentFragment);
+           });
+
+           handleAmountOptionClick(binding.firstAmountOptionCv, FIRST_AMOUNT_OPTION);
+           handleAmountOptionClick(binding.secondAmountOptionCv, SECOND_AMOUNT_OPTION);
+           handleAmountOptionClick(binding.thirdAmountOptionCv, THIRD_AMOUNT_OPTION);
+
+//           t1= view.findViewById(R.id.amountEt);
+//           String amount = t1.getText().toString();
+//           Log.d("SendingFragment", "Amount value: " + amount);
+//           Bundle bundle = new Bundle();
+//           bundle.putString("amount", amount);
+//           MoneySentFragment fragment = new MoneySentFragment();
+//           fragment.setArguments(bundle);
+
+           return view;
+    }
+
+
+    private void handleAmountOptionClick(MaterialCardView amountOptionCv, String amount) {
+        amountOptionCv.setOnClickListener(unused -> binding.amountEt.setText(amount));
+    }
+}
---
+++ app/src/main/java/com/example/wallety/model/Task.java
@@ -0,0 +1,71 @@
+package com.example.wallety.model;
+
+public class Task {
+    private String id;
+    private String name;
+    private String desc;
+    private String date;
+    private String time;
+    private String amount;
+
+
+    public Task() {
+    }
+
+    public Task(String id, String name, String desc, String date, String time, String amount) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.date = date;
+        this.time = time;
+        this.amount = amount;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
